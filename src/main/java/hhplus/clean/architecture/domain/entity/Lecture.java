8c439ed3e93c6d23419fd3d79403b758487889df
honesty2223 <honesty220203@gmail.com>
2024-06-27 21:46:23 +0900

    초기 데이터, 엔티티 수정

--- src/main/java/hhplus/clean/architecture/domain/entity/Lecture.java
+++ src/main/java/hhplus/clean/architecture/domain/entity/Lecture.java
@@ -1,9 +1,11 @@
 package hhplus.clean.architecture.domain.entity;
 
+import hhplus.clean.architecture.application.exception.LectureAlreadyFullException;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 import java.time.LocalDate;
 
@@ -25,9 +27,21 @@ public class Lecture {
     private LocalDate lectureDate;
 
     @Column(name = "max_capacity")
+    @ColumnDefault("30")
     private int maxCapacity;
 
     @Column(name = "current_enrollment")
+    @ColumnDefault("0")
     private int currentEnrollment;
 
+    /**
+     * 특강의 정원이 초과되었는지 확인하고, 초과되었을 경우 예외를 발생시킴
+     *
+     * @throws LectureAlreadyFullException 특강의 정원이 초과된 경우
+     */
+    public void validateLectureCapacity() {
+        if (this.getCurrentEnrollment() >= this.getMaxCapacity()) {
+            throw new LectureAlreadyFullException("해당 특강의 정원이 초과되었습니다: " + this.getLectureId());
+        }
+    }
 }
